6c72c46b85a4cdb059b551f90bc54f4e01d686d3
paulisaenz <sae21002@byui.edu>
2022-05-18 17:20:48 -0300

    Lesson 5

---
+++ lesson5/dom_manipulation/js/script.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('list');
+
+button.addEventListener('click', function() {
+    const myItem = button.value;
+    input.value = '';
+
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+
+    listItem.appendChild(listText);
+    listText.textContent = myItem;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = '❌';
+    list.appendChild(listItem);
+
+    listBtn.addEventListener('click', function() {
+    list.removeChild(listItem);
+    });
+
+    input.focus();
+});
