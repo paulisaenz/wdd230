3b5dfcc2dfcff96e8513bc6a036b53c1e1666f46
paulisaenz <sae21002@byui.edu>
2022-05-18 19:51:07 -0300

    Mistakes found

--- lesson5/dom_manipulation/js/script.js
+++ lesson5/dom_manipulation/js/script.js
@@ -1,6 +1,6 @@
-const input = document.querySelector('input');
-const button = document.querySelector('button');
-const list = document.querySelector('list');
+const input = document.querySelector('#input');
+const button = document.querySelector('#button');
+const list = document.querySelector('#list');
 
 button.addEventListener('click', function() {
     const myItem = button.value;
