afa3acbc0507569fa550b4745d63abe6132214ab
paulisaenz <sae21002@byui.edu>
2022-05-18 20:35:17 -0300

    Some changes

--- lesson5/dom_manipulation/js/dinamic-soppinglist.js
+++ lesson5/dom_manipulation/js/dinamic-soppinglist.js
@@ -1,17 +1,19 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('#button');
-const list = document.querySelector('#list');
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
     const myItem = input.value;
     input.value = '';
 
     const listItem = document.createElement('li');
+    const listText = document.createElement('span');
     const listBtn = document.createElement('button');
 
+    listItem.appendChild(listText);
     listText.textContent = myItem;
     listItem.appendChild(listBtn);
-    listBtn.textContent = '❌';
+    listBtn.textContent = 'Delete';
     list.appendChild(listItem);
 
     listBtn.addEventListener('click', () => {
