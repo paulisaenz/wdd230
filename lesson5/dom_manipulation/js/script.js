80da43f65d533ec839e2c834c0482ae61b4ccd51
paulisaenz <sae21002@byui.edu>
2022-05-18 21:18:24 -0300

    JS BOM problem solved

--- lesson5/dom_manipulation/js/script.js
+++ lesson5/dom_manipulation/js/script.js
@@ -1,20 +1,20 @@
 const input = document.querySelector('#favchap');
-const button = document.querySelector('#button');
-const list = document.querySelector('#list');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
 
-button.addEventListener('click', () => {
+button.addEventListener('click', function() {
     const myItem = input.value;
     input.value = '';
 
     const listItem = document.createElement('li');
     const listBtn = document.createElement('button');
 
-    listText.textContent = myItem;
+    listItem.textContent = myItem;
     listItem.appendChild(listBtn);
     listBtn.textContent = '❌';
     list.appendChild(listItem);
 
-    listBtn.addEventListener('click', () => {
+    listBtn.addEventListener('click', function() {
     list.removeChild(listItem);
     });
 
