8ee234b89b77ffdbf88f8ed3eac73cc604ee9fa2
paulisaenz <sae21002@byui.edu>
2022-06-04 22:53:20 -0300

    Still having issues with the images

--- chamber2/js/script.js
+++ chamber2/js/script.js
@@ -39,3 +39,43 @@ if (d === 1 || d === 2) {
 // close.addEventListener("click", function() {
 //         banner.style.display = "none";
 // });
+
+// initialize display elements
+// const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// get the number of milliseconds (date in milliseconds) the last time the page was visited
+let numMills = Number(window.localStorage.getItem("mills-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+
+// store the new number of visits value (writing/saving the new number to the local storage)
+// localStorage.setItem("visits-ls", numVisits);
+
+// // show number of days //
+// // if the number of milliseconds does not = 0
+// if (numMills !== 0) {  
+// 	// then do this math equation and round to a whole number which will now = (NumDays)
+//     numDays = Math.round((Date.now() - numMills) / 86400000); 
+// 	// now go to the todayDisplay and retrieve todays date and diplay a "text" number we 
+// 	// will call numDays 
+// 	todayDisplay.textContent = numDays;
+
+// } else {
+// 	todayDisplay.textContent = `This is your first visit!`;
+// }
+
+// store the current date in number of Milliseconds 
+// numMills = Date.now();
+// localStorage.setItem("mills-ls", numMills);
