19ab3495b44f28912c1be72851f08ae52d0c85f3
paulisaenz <sae21002@byui.edu>
2022-06-22 16:14:21 -0300

    Weather API Activity Done

---
+++ lesson10/weatherapi.js
@@ -0,0 +1,45 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+// API URL
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=0f9b95d11a5a00e2312ffdb8bd89daea";
+
+// Asynchonus function with fetch requesting the API URL
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      //console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();
+
+// Weather and images
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(
+    0
+  )}</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  captionDesc.innerHTML = desc;
+}
+
+// Capialize the figcaption
+
+document.querySelector("figcaption").style.textTransform = "capitalize";
